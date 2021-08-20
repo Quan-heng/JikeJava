3a039cf7040d5edde65ed87866280cb4b0cc337c
quan <15364446581@163.com>
2021-08-20 14:36:22 +0800

    refactor some code

--- week03/src/main/java/io/quanheng/server/BackendServer02.java
+++ week03/src/main/java/io/quanheng/server/BackendServer02.java
@@ -28,7 +28,7 @@ public class BackendServer02 {
             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
             printWriter.println("HTTP/1.1 200 OK");
             printWriter.println("Content-Type:text/html;charset=utf-8");
-            String body = "hello,nio3";
+            String body = "server02: hello,nio3";
             printWriter.println("Content-Length:" + body.getBytes().length);
             printWriter.println();
             printWriter.write(body);
