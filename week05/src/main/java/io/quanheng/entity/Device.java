ca70668c2cc5155edaed0f72387205ed1352de3c
quanheng <15364446581@163.com>
2021-09-07 22:53:20 +0800

    starteR

--- week05/src/main/java/io/quanheng/entity/Device.java
+++
@@ -1,9 +0,0 @@
-package io.quanheng.entity;
-
-import lombok.Data;
-
-@Data
-public class Device {
-    private String id;
-    private String name;
-}
---
+++ week05/src/test/java/HomeWork10.java
@@ -0,0 +1,16 @@
+package io.quanheng.test;
+
+import com.quan.MySchool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class HomeWork10 {
+    @Autowired
+    MySchool mySchool;
+
+    @Test
+    public  void testStarter() {
+        System.out.println(myS);
+    }
+}
